Corrected waiting time calculation to use selected agent

Part 3 datasets/simulated_annealing_real_data_with_rostering.py
@@ -99,7 +99,7 @@ def good_performance_periods(shift_schedule_per_day, call_centre_scenario, max_w
                     # Checks that call can be answered before customer runs out of patience and that call arrives before agent shift has ended
                     # also check that the call arrives before the agent leaves for break or that the agent is back before the customer drops
                     if (agent_logs.iloc[j,0] <= call_features.iloc[0,0] + call_features.iloc[0,2]) and (agent_logs.iloc[j,1] > call_features.iloc[0,0]) and ((agent_logs.iloc[j,2] > call_features.iloc[0,0]) or (agent_logs.iloc[j,3] <= call_features.iloc[0,0] + call_features.iloc[0,2])):
-                        call_features = call_features.assign(Waiting_time = np.maximum(agent_logs.iloc[0,0] - call_features.iloc[0,0],0))
+                        call_features = call_features.assign(Waiting_time = np.maximum(agent_logs.iloc[j,0] - call_features.iloc[0,0],0))
                         call_features = call_features.assign(Call_answered = 'yes')
                         call_centre_scenario_operation = call_centre_scenario_operation.append(call_features)
                         agent_logs = update_agent_times(agent_logs, call_features, j)