d43c2d3f927c90c8e1cc3cb8a5eb96478316d092
Liketso Nthimo <u13331290@tuks.co.za>
2021-01-28 13:25:38 +0200

    Tested some optimisations on the test code

--- Part 1 datasets/test code 1 import.py
+++ Part 1 datasets/test code 1 import.py
@@ -13,8 +13,9 @@ from datetime import datetime
 import multiprocessing
 
 def calculate_cost(shift_schedule):
-    rows = shift_schedule.shape[0]
+    '''rows = shift_schedule.shape[0]
     total_cost = 0
+    
     
     for i in range(rows):
         shift_start = shift_schedule.iloc[i,1]
@@ -25,7 +26,10 @@ def calculate_cost(shift_schedule):
         
         shift_cost = (shift_end - shift_start - (shift_break_end - shift_break_start)) * shift_staff_number
         total_cost = total_cost + shift_cost
+    '''
     
+    total_cost = (shift_schedule['End_time'] - shift_schedule['Start_time'] - (shift_schedule['Break_end'] - shift_schedule['Break_start'])) * shift_schedule['Staff_number']
+    total_cost = total_cost.sum()
     return total_cost
 
 def generate_agent_logs(shift_schedule):
@@ -52,21 +56,39 @@ def update_break_periods(agent_logs, arrival_time):
     agent_logs = agent_logs.sort_index()
     return agent_logs
 
-
+'''
 def update_agent_times(agent_logs, call_features, agent_index):
     agent_logs = agent_logs.copy()
     arrival_time = call_features.iloc[0,0]
     agent_logs = update_break_periods(agent_logs, arrival_time)
     #print('AT: ' + str(arrival_time*60*60) + ', SD: ' + str(call_features.iloc[0,1]*60*60) + ', ST: ' + str(agent_logs.iloc[agent_index,0]*60*60) + ', ET: ' + str(agent_logs.iloc[agent_index,1]*60*60))
     agent_logs.iloc[agent_index,0] = arrival_time + call_features.iloc[0,1]
+    ' ''
     if agent_logs.iloc[agent_index,0] > agent_logs.iloc[agent_index,1]:
         #agent_logs = agent_logs.iloc[1:,]
         agent_logs = agent_logs.drop(agent_logs.index[agent_index])
+        ' ''
+    agent_logs = agent_logs.loc[(agent_logs['Start_time'] < agent_logs['End_time']) & (agent_logs['End_time'] > arrival_time),]
+        
+    agent_logs = agent_logs.sort_values(by = ['Start_time', 'End_time', 'Break_start', 'Break_end'])
+    agent_logs = agent_logs.reset_index(drop = True)
+    return agent_logs
+'''
+
+
+def update_agent_times(agent_logs, call_features, agent_index):
+    agent_logs = agent_logs.copy()
+    arrival_time = call_features['Arrival_time']
+    agent_logs = update_break_periods(agent_logs, arrival_time)
+    
+    agent_logs.iloc[agent_index,0] = arrival_time + call_features['Service_duration']
+    agent_logs = agent_logs.loc[(agent_logs['Start_time'] < agent_logs['End_time']) & (agent_logs['End_time'] > arrival_time),]
         
     agent_logs = agent_logs.sort_values(by = ['Start_time', 'End_time', 'Break_start', 'Break_end'])
     agent_logs = agent_logs.reset_index(drop = True)
     return agent_logs
 
+'''
 def populate_performance_info(shift_schedule_per_day, call_centre_scenario, max_wait):
     call_centre_scenario_operation = pd.DataFrame()
     per_day_performance_info = pd.DataFrame()
@@ -162,7 +184,112 @@ def populate_performance_info(shift_schedule_per_day, call_centre_scenario, max_
     total_performance_info = pd.DataFrame(append_day_features)
 
     return pd.concat([per_day_performance_info, total_performance_info], axis = 1)
+'''
+
+def populate_performance_info(shift_schedule_per_day, call_centre_scenario, max_wait):
+    call_centre_scenario_operation = pd.DataFrame()
+    per_day_performance_info = pd.DataFrame()
+    unique_days = call_centre_scenario['Day'].unique()
+    service_level = 0.8
+    total_met_sl_periods = 0
+    total_cost = 0
+    total_answered_calls = 0
+    total_dropped_calls = 0
+    start = datetime.now()
     
+    for day in unique_days:
+        day_shift_schedule = shift_schedule_per_day[shift_schedule_per_day['Day'] == day].copy()
+        day_cost = calculate_cost(day_shift_schedule)
+        del day_shift_schedule['Day']
+        day_shift_schedule = day_shift_schedule.reset_index(drop = True)
+        
+        day_answered_calls = 0
+        day_dropped_calls = 0
+        day_good_periods = 0
+
+        agent_logs = generate_agent_logs(day_shift_schedule)
+        
+        day_scenario = call_centre_scenario[call_centre_scenario['Day'] == day]
+        
+        waiting_time_list = []
+        call_answered_list = []
+        for index, call_features in day_scenario.iterrows():
+            
+            if agent_logs.shape[0] > 0:
+                agent_rows = agent_logs.shape[0]
+                #print('agent rows: ',agent_rows)
+                for j in range(agent_rows):
+                    is_unanswered = True
+                    # Checks that call can be answered before customer runs out of patience and that call arrives before agent shift has ended
+                    # also check that the call arrives before the agent leaves for break or that the agent is back before the customer drops
+                    if (agent_logs.iloc[j,0] <= call_features['Arrival_time'] + call_features['Time_to_abandon']) and (agent_logs.iloc[j,1] > call_features['Arrival_time']) and ((agent_logs.iloc[j,2] > call_features['Arrival_time']) or (agent_logs.iloc[j,3] <= call_features['Arrival_time'] + call_features['Time_to_abandon'])):
+                        waiting_time_list.append(np.maximum(agent_logs.iloc[j,0] - call_features['Arrival_time'],0))
+                        call_answered_list.append('yes')
+                        agent_logs = update_agent_times(agent_logs, call_features, j)
+                        day_answered_calls = day_answered_calls + 1
+                        total_answered_calls = total_answered_calls + 1
+                        is_unanswered = False
+                        break
+                    
+                if is_unanswered:
+                    waiting_time_list.append(call_features['Time_to_abandon'])
+                    call_answered_list.append('no')
+                    day_dropped_calls = day_dropped_calls + 1
+                    total_dropped_calls = total_dropped_calls + 1
+                
+            else:
+                waiting_time_list.append(call_features['Time_to_abandon'])
+                call_answered_list.append('no')
+                day_dropped_calls = day_dropped_calls + 1
+                total_dropped_calls = total_dropped_calls + 1
+        
+        day_scenario = day_scenario.assign(Waiting_time = waiting_time_list)
+        day_scenario = day_scenario.assign(Call_answered = call_answered_list)
+        #day_scenario['Waiting_time'] = waiting_time_list
+        #day_scenario['Call_answered'] = call_answered_list
+        
+        call_centre_scenario_operation = day_scenario.copy()
+    
+        for performance_interval in range(1,73):
+            performance_dataset = call_centre_scenario_operation[call_centre_scenario_operation['Performance_interval'] == performance_interval]
+            all_eligible_calls_dataset = performance_dataset.loc[(performance_dataset['Waiting_time'] >= 5/60/60) | (performance_dataset['Call_answered'] == 'yes')]
+            service_level_dataset = performance_dataset.loc[(performance_dataset['Waiting_time'] <= max_wait) & (performance_dataset['Call_answered'] == 'yes')]
+            all_calls = all_eligible_calls_dataset.shape[0]
+            service_level_calls = service_level_dataset.shape[0]
+            
+            if all_calls == 0:
+                total_met_sl_periods = total_met_sl_periods + 1
+                day_good_periods = day_good_periods + 1
+            
+            else:
+                if service_level_calls / all_calls >= service_level:
+                    total_met_sl_periods = total_met_sl_periods + 1
+                    day_good_periods = day_good_periods + 1
+        
+        total_cost = total_cost + day_cost
+        append_day_features = np.zeros((1,5))
+        
+        append_day_features[0,0] = day
+        append_day_features[0,1] = day_good_periods
+        append_day_features[0,2] = day_cost
+        append_day_features[0,3] = day_answered_calls
+        append_day_features[0,4] = day_dropped_calls
+        
+        per_day_performance_info = per_day_performance_info.append(pd.DataFrame(append_day_features))
+        print('Day ',day , 'appended at: ', datetime.now() - start)
+    
+    append_day_features = np.zeros((1,4))
+    
+    append_day_features[0,0] = total_met_sl_periods
+    append_day_features[0,1] = total_cost
+    append_day_features[0,2] = total_answered_calls
+    append_day_features[0,3] = total_dropped_calls
+
+    total_performance_info = pd.DataFrame(append_day_features)
+
+    return pd.concat([per_day_performance_info, total_performance_info], axis = 1)
+    
+
 
 '''
 def cycle_number_to_shift_day(shift_schedule, day_range):
@@ -189,7 +316,7 @@ def cycle_number_to_shift_day(shift_schedule, day_range):
 '''
 
 
-sample_size = 6
+sample_size = 5
 day_range = range(1,sample_size)
 
 max_wait_sec = 20
